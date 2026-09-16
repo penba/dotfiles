088ee4b38bac1d5739254ee325f1eb010e306fd2
asdf <258933500+penba@users.noreply.github.com>
2026-09-16 14:35:24 -0400

    Initial dotfiles

---
+++ .config/nvim/init.lua
@@ -0,0 +1,31 @@
+vim.o.scrolloff = 10
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.showmode = false
+vim.o.signcolumn = "yes"
+
+vim.o.expandtab = false
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.smarttab = true
+
+vim.o.swapfile = false
+vim.o.clipboard = "unnamedplus"
+vim.o.backup = false
+vim.o.swapfile = false
+vim.o.undofile = false
+
+vim.o.wrap = true
+vim.o.breakindent = true
+vim.o.linebreak = true
+
+vim.g.mapleader = " "
+
+vim.pack.add({
+	{src = "https://github.com/aktersnurra/no-clown-fiesta.nvim"}, 
+})
+
+vim.cmd.colorscheme("no-clown-fiesta")
