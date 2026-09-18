a05b2684a20a93e369a9f113e5c23891d2e1edd5
penba <258933500+penba@users.noreply.github.com>
2026-09-17 23:24:10 -0400

    _ > -

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ end
 
 set -g fish_greeting
 
-function safe-stow
+function safe_stow
 	set git_root (git rev-parse --show-toplevel 2>/dev/null)
 	and string match -q "dotfiles" $git_root
 	or { echo "Not in dotfiles repo"; return 1; }
@@ -13,6 +13,6 @@ end
 abbr -a ls ls -A
 abbr -a ll ls -Al
 abbr -a vim nvim
-abbr -a stow safe-stow
+abbr -a stow safe_stow
 
 zoxide init --cmd cd fish | source
