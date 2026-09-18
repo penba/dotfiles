322839bf6f126cf610abe342b83aa08d4515583c
penba <258933500+penba@users.noreply.github.com>
2026-09-17 23:16:49 -0400

    Added safe-stow function and abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,15 @@ end
 
 set -g fish_greeting
 
+function safe-stow
+	set git_root (git rev-parse --show-toplevel 2>/dev/null)
+	and string match -q "dotfiles" $git_root
+	or { echo "Not in dotfiles repo"; return 1; }
+end
+
 abbr -a ls ls -A
+abbr -a ll ls -Al
+abbr -a vim nvim
+abbr -a stow safe-stow
 
 zoxide init --cmd cd fish | source
