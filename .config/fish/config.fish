266388507a75f5376ed6fdea645ab1c9d000221a
penba <258933500+penba@users.noreply.github.com>
2026-09-19 03:43:29 -0400

    Too many changes I can't be bothered

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,15 +4,9 @@ end
 
 set -g fish_greeting
 
-function safe_stow
-	set git_root (git rev-parse --show-toplevel 2>/dev/null)
-	and string match -q "dotfiles" $git_root
-	or { echo "Not in dotfiles repo"; return 1; }
-end
-
 abbr -a ls ls -A
 abbr -a ll ls -Al
 abbr -a vim nvim
-abbr -a stow safe_stow
+abbr -a stow stow -t \~
 
 zoxide init --cmd cd fish | source
