4c1c4f711ea1c0bebc64dfdff195fe5b0b2ce1de
penba <258933500+penba@users.noreply.github.com>
2026-09-19 01:47:33 -0400

    Changed colorscheme to kanagawa dragon

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,11 +25,11 @@ vim.o.linebreak = true
 vim.g.mapleader = " "
 
 vim.pack.add({
-	{ src = "https://github.com/aktersnurra/no-clown-fiesta.nvim" }, 
+	{ src = "https://github.com/rebelot/kanagawa.nvim" }, 
 	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
 })
 
-vim.cmd.colorscheme("no-clown-fiesta")
+vim.cmd.colorscheme("kanagawa-dragon")
 
 require("lualine").setup {
 	options = {
